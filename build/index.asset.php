f37340c82dfd3174e4a667e702f6ce431d4fef43
Shreyash <sdwadmalwar@gmail.com>
2023-08-09 14:41:41 +0530

    pushing final

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'f5e6b814e1de75768b5b381e22df3b7f');
+<?php return array('dependencies' => array(), 'version' => 'c62f4cf4887d19aef4e4');
--- src/edit.js
+++ src/edit.js
@@ -1,11 +1,33 @@
 import { __ } from '@wordpress/i18n';
+import { TextControl } from '@wordpress/components';
+import { useSelect } from '@wordpress/data';
+import { useEntityProp } from '@wordpress/core-data';
 import { useBlockProps } from '@wordpress/block-editor';
 import './editor.scss';
 
 export default function Edit() {
+	const postType = useSelect((select) => {
+		return select('core/editor').getCurrentPostType();
+	}, []);
+
+	const [meta, setMeta] = useEntityProp('postType', postType, 'meta');
+	const subTitleValue = meta._blocks_course_post_subtitle;
+
+	const onSubtitleChange = (value) => {
+		setMeta({ ...meta, _blocks_course_post_subtitle: value });
+	};
+
 	return (
-		<p {...useBlockProps()}>
-			{__('Boilerplate – hello from the editor!', 'boilerplate')}
-		</p>
+		<div {...useBlockProps()}>
+			{subTitleValue || subTitleValue === '' ? (
+				<TextControl
+					label={__('Post Subtitle', 'metadata-block')}
+					value={subTitleValue}
+					onChange={onSubtitleChange}
+				/>
+			) : (
+				__('Meta Field is Not Registered', 'metadata-block')
+			)}
+		</div>
 	);
 }
